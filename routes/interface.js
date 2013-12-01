e70c263db49fba70c08dcdfb1b2e457a2b724659
witmob <1032860079@qq.com>
2013-12-01 15:48:44 +0800

    半透明层

--- routes/interface.js
+++ routes/interface.js
@@ -5,39 +5,39 @@
  * Time: 下午2:53
  * To change this template use File | Settings | File Templates.
  */
-exports.action = function(req, res){
-    var body=req.body;
-    console.log('interface body=',body);
+exports.action = function (req, res) {
+    var body = req.body;
+    console.log('interface body=', body);
     console.log(body.command);
-    if(body&&body.command){
+    if (body && body.command) {
         this[body.command](req, res);
-    }else{
+    } else {
         //错误跳到 首页面
-        sendData(res,{code:1,message:"命令错误,没有command"});
+        sendData(res, {code: 1, message: "命令错误,没有command"});
     }
 };
 //发送数据
-var sendData=function(res, data){
+var sendData = function (res, data) {
     res.send(data);
 }
-var picManager=require('../lib/PicManager.js');
-uploadPic=function(req, res){
-    picManager.action(req,function(data){
-        sendData(res,data);
+var picManager = require('../lib/PicManager.js');
+uploadPic = function (req, res) {
+    picManager.action(req, function (data) {
+        sendData(res, data);
     });
 }
 
 //用户信息
-var userManager=require('../lib/UserManager.js');
-getUserList=function(req, res){
-    userManager.getUserList(req,function(data){
-        sendData(res,data);
+var userManager = require('../lib/UserManager.js');
+getUserList = function (req, res) {
+    userManager.getUserList(req, function (data) {
+        sendData(res, data);
     });
 }
 
-datesearch=function(req,res){
-    userManager.datesearch(req,function(data){
-        sendData(res,data);
+datesearch = function (req, res) {
+    userManager.datesearch(req, function (data) {
+        sendData(res, data);
 
     });
 }
--- routes/manageserver.js
+++ routes/manageserver.js
@@ -5,27 +5,27 @@
  * Time: 下午2:55
  * To change this template use File | Settings | File Templates.
  */
-exports.action = function(req, res){
-    var command=req.param('command');
-    console.log('command>>>',command);
-    if(command){
+exports.action = function (req, res) {
+    var command = req.param('command');
+    console.log('command>>>', command);
+    if (command) {
         this[command](req, res);
-    }else{
+    } else {
         //错误跳到 首页面
         res.redirect("v?command=home");
     }
 };
 var path = require('path');
 //首页 页面
-home=function(req, res){
+home = function (req, res) {
     res.sendfile(path.join(__dirname, '../view/home.html'));
 }
 //图片上传测试
-test=function(req,res){
+test = function (req, res) {
     res.sendfile(path.join(__dirname, '../view/test.html'));
 }
 //得到图片流
-var picManager=require('../lib/PicManager.js');
-pic=function(req,res){
-    picManager.getPic(req,res);
+var picManager = require('../lib/PicManager.js');
+pic = function (req, res) {
+    picManager.getPic(req, res);
 }
